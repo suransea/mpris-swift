a4137c4338c93cf3469fe245306b55717891f7c3
sea <simpleslight@icloud.com>
2024-10-03 19:15:54 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Mpris",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Mpris",
+      targets: ["Mpris"])
+  ],
+  dependencies: [
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Mpris"),
+    .testTarget(
+      name: "MprisTests",
+      dependencies: ["Mpris"]
+    ),
+  ]
+)
